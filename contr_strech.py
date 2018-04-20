ac612d8a8f2f6df8e326314e96b88d0d9776add2
kjans123 <kj123@duke.edu>
2018-04-20 13:05:45 -0400

    added contrast stretching function. Includes docstrings, error catching, logging and tests. Modifired histo_equal to include one extra print statement.

--- contr_strech.py
+++ contr_strech.py
@@ -1,10 +1,11 @@
-def histo_equal(base64String):
-    """"function that takes in a base64 string, converts to img array
-        and then runs a global histogram equalization process on each
-        channel in the img array.
+def contr_stretch(base64String):
+    """"function that intakes an image as base64 string. Converts to np
+        array and runs contrast stretching on array. Returns image as
+        base64 string. For min and max of contrast stretching, takes
+        values in 40th and 95th percentile respectively.
 
     :param base64string: takes as input a JPG base64 string from app front-end
-    :returns img64: returns a post-processed JPG img as base64 string
+    :returns imgString: returns a post-processed JPG img as base64 string
     :raises ValueError: raises Value Error if base64 string is empty
     :raises ImportError: raises Import Error if numpy or scikit packages
                          are not found. Also raises Import Error if
@@ -16,21 +17,22 @@ def histo_equal(base64String):
                         format='%(levelname)s %(asctime)s %(message)s',
                         datefmt='%m/%d/%Y %I:%M:%S %p', level=str1)
     try:
-        if base64String is None or base64String == "":
-            logging.warning("base64String is EMPTY")
-            raise ValueError("empty base64String")
         from skimage import exposure
         import numpy as np
         try:
             from base64_conv_numpy import convert_image_to_np_array
             from base64_conv_numpy import convert_processed_np_array_to_base64
+            if base64String is None or base64String == "":
+                logging.warning("base64String is EMPTY")
+                raise ValueError("empty base64String")
             imgArray, a_type, m, w, z = convert_image_to_np_array(base64String)
-            for c in range(imgArray.shape[2]):
-                imgArray[:, :, c] = exposure.equalize_hist(imgArray[:, :, c])
-            img64 = convert_processed_np_array_to_base64(imgArray)
-            m = "success: processed (histogram eq.) img and returned as base64"
+            v1, v2 = np.percentile(imgArray, (0.4, 95))
+            imgC = exposure.rescale_intensity(imgArray, in_range=(v1, v2))
+            imgString = convert_processed_np_array_to_base64(imgC)
+            print(imgString)
+            m = "success: processed (contr. str.) img and returned as base64"
             logging.info(m)
-            return img64
+            return imgString
         except ImportError:
             msg = 'base64_conv_numpy module not found.'
             print(msg)
