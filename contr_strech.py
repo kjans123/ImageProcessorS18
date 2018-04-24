db452c81ea508afd3039cfae7bdcd9c5e70a3488
kjans123 <kj123@duke.edu>
2018-04-24 12:25:21 -0400

    removed if __name__ == __main__ block

--- contr_strech.py
+++ contr_strech.py
@@ -41,22 +41,3 @@ def contr_stretch(base64String):
         msgg = 'scikit/numpy packages not found. Check virtualenv'
         print(msgg)
         logging.warning(msgg)
-
-if __name__ == "__main__":
-    from skimage.util import img_as_ubyte
-    from skimage import exposure
-    from skimage.morphology import disk
-    from skimage.filters import rank
-    import numpy as np
-    import base64
-    import PIL
-    from PIL import Image
-    from base64_conv_numpy import convert_image_to_np_array
-    from base64_conv_numpy import convert_processed_np_array_to_base64
-    from base64_conv_numpy import encode_image_string
-    imgString = encode_image_string('tiny.jpg')
-    proc_image = contr_stretch(imgString)
-    # img64 = convert_processed_np_array_to_base64(proc_image)
-    with open('test.jpg', 'wb') as image_file:
-        write_image = base64.b64decode(proc_image)
-        image_file.write(write_image)
