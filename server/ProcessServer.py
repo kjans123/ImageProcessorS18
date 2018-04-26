3bf1f303b1d2d2d644f67fc8cd02ae7c33366eaf
kjans123 <kj123@duke.edu>
2018-04-26 15:31:42 -0400

    added image functions to server file. pep8 styled

--- server/ProcessServer.py
+++ server/ProcessServer.py
@@ -39,7 +39,6 @@ def process():
     %(message)s', datefmt='%m/%d/%Y %I:%M:%S %pi')
     logging.info("Begin app route to /process")
     info = request.get_json()
-    # Make sure the json received is correct
     try:
         email = info["user_email"]
     except KeyError:
@@ -67,42 +66,24 @@ def process():
     except TypeError:
         return jsonify("email is not string"), 400
         print("Please provide information in string format!")
-    # if cases that will direct to the correct processing method
-    # it would be better to import these methods from a separate file
     current_time = datetime.datetime.now()
-    processed_list = []   # this is the list of strings of processed images
+    processed_list = []
     processed_histograms = []
     pre_img_list = []
     pre_img_histograms = []
     for i, img in enumerate(pre_img):
-        # for loop to go through all images
-        # save user image with correct number
-        # add method from main.py
-        # save_image
         if method == "histeq":
             imgArray, a_type, m, w, z = convert_image_to_np_array(img)
-            hist_equal_image = histo_equal(imgArray)
+            hist_image = histo_equal(imgArray)
             histogram_of_pre_img = create_histo(imgArray)
             histogram_of_post_img = create_histo(hist_equal_image)
-            hist_equal_img64 = convert_processed_np_array_to_base64(hist_equal_image)
-            processed_list.append(hist_equal_img64)
+            hist_img64 = convert_processed_np_array_to_base64(hist_image)
+            processed_list.append(hist_img64)
             pre_img_list.append(pre_img)
             processed_histograms.append(histogram_of_post_img)
             pre_img_histograms.append(histogram_of_pre_img)
             return_size = (str(m)+str(w) + ' pixels')
-            # Add function for histogram equalization
-                # input is pre_img (depending on scikit or whatever,
-                # may need to convert format then back to a b64 image string)
-                # output is post_img
-            # post_img = FOOBAR NOW, post_img should be list of strings
-            # Once complete, save user action to database
-                # remember we need to identify by __id or something
-                # instead of adding with just email
-                # add_hist from main.py
-            # save processed b64 string
-                # adds to the list of processed image strings in b64
-            processed_list.append(post_img)
-            if i == len(pre_img) - 1:  # last image in list
+            if i == len(pre_img) - 1:
                 new_time = datetime.datetime.now()
                 duration = new_time - current_time
                 new_info = {
@@ -116,34 +97,20 @@ def process():
                     "upload_time": time2str(current_time)
                     "pic_size": return_size
                 }
-                # need to add this list into some tmp folder
-                # create_tmp function with json
-                # input is (processed_list)
                 return jsonify(new_info)
         elif method == "stretch":
             imgArray, a_type, m, w, z = convert_image_to_np_array(img)
-            hist_equal_image = contr_stretch(imgArray)
+            hist_image = contr_stretch(imgArray)
             histogram_of_pre_img = create_histo(imgArray)
             histogram_of_post_img = create_histo(hist_equal_image)
-            hist_equal_img64 = convert_processed_np_array_to_base64(hist_equal_image)
-            processed_list.append(hist_equal_img64)
+            hist_img64 = convert_processed_np_array_to_base64(hist_image)
+            processed_list.append(hist_img64)
             pre_img_list.append(pre_img)
             processed_histograms.append(histogram_of_post_img)
             pre_img_histograms.append(histogram_of_pre_img)
             return_size = (str(m)+str(w) + ' pixels')
-            # Add function for contrast stretching
-            # input is pre_img (depending on scikit or whatever,
-                # may need to convert format then back to a b64 image string)
-                # output is post_img
-            # post_img = FOOBAR NOW, post_img should be list of strings
-            # Once complete, save user action to database
-                # remember we need to identify by __id or something
-                # instead of adding with just email
-                # add_hist from main.py
-            # save processed b64 string
-                # adds to the list of processed image strings in b64
-            processed_list.append(post_img)
-            if i == len(pre_img) - 1:  # last image in list
+            return_size = (str(m)+str(w) + ' pixels')
+            if i == len(pre_img) - 1:
                 new_time = datetime.datetime.now()
                 duration = new_time - current_time
                 new_info = {
@@ -157,34 +124,19 @@ def process():
                     "upload_time": time2str(current_time)
                     "pic_size": return_size
                 }
-                # need to add this list into some tmp folder
-                # create_tmp function with json
-                # input is (processed_list)
                 return jsonify(new_info)
         elif method == "logcomp":
             imgArray, a_type, m, w, z = convert_image_to_np_array(img)
-            hist_equal_image = logComp(imgArray)
+            hist_image = logComp(imgArray)
             histogram_of_pre_img = create_histo(imgArray)
             histogram_of_post_img = create_histo(hist_equal_image)
-            hist_equal_img64 = convert_processed_np_array_to_base64(hist_equal_image)
-            processed_list.append(hist_equal_img64)
+            hist_img64 = convert_processed_np_array_to_base64(hist_image)
+            processed_list.append(hist_img64)
             pre_img_list.append(pre_img)
             processed_histograms.append(histogram_of_post_img)
             pre_img_histograms.append(histogram_of_pre_img)
             return_size = (str(m)+str(w) + ' pixels')
-            # Add function for log compression
-            # input is pre_img (depending on scikit or whatever,
-                # may need to convert format then back to a b64 image string)
-                # output is post_img
-            # post_img = FOOBAR NOW, post_img should be list of strings
-            # Once complete, save user action to database
-                # remember we need to identify by __id or something
-                # instead of adding with just email
-                # add_hist from main.py
-            # save processed b64 string
-                # adds to the list of processed image strings in b64
-            processed_list.append(post_img)
-            if i == len(pre_img) - 1:  # last image in list
+            if i == len(pre_img) - 1:
                 new_time = datetime.datetime.now()
                 duration = new_time - current_time
                 new_info = {
@@ -198,34 +150,19 @@ def process():
                     "upload_time": time2str(current_time)
                     "pic_size": return_size
                 }
-                # need to add this list into some tmp folder
-                # create_tmp function with json
-                # input is (processed_list)
                 return jsonify(new_info)
         elif method == "reverse":
             imgArray, a_type, m, w, z = convert_image_to_np_array(img)
-            hist_equal_image = logComp(imgArray)
-            histogram_of_pre_img = reverseVid(imgArray)
+            hist_image = reverseVid(imgArray)
+            histogram_of_pre_img = create_histo(imgArray)
             histogram_of_post_img = create_histo(hist_equal_image)
-            hist_equal_img64 = convert_processed_np_array_to_base64(hist_equal_image)
-            processed_list.append(hist_equal_img64)
+            hist_img64 = convert_processed_np_array_to_base64(hist_image)
+            processed_list.append(hist_img64)
             pre_img_list.append(pre_img)
             processed_histograms.append(histogram_of_post_img)
             pre_img_histograms.append(histogram_of_pre_img)
             return_size = (str(m)+str(w) + ' pixels')
-            # Add function for reverse video
-            # input is pre_img (depending on scikit or whatever,
-                # may need to convert format then back to a b64 image string)
-                # output is post_img
-            # post_img = FOOBAR NOW, post_img should be list of strings
-            # Once complete, save user action to database
-                # remember we need to identify by __id or something
-                # instead of adding with just email
-                # add_hist from main.py
-            # save processed b64 string
-                # adds to the list of processed image strings in b64
-            processed_list.append(post_img)
-            if i == len(pre_img) - 1:  # last image in list
+            if i == len(pre_img) - 1:
                 new_time = datetime.datetime.now()
                 duration = new_time - current_time
                 logging.info("Create json to be sent to frontend for preview")
@@ -240,9 +177,6 @@ def process():
                     "upload_time": time2str(current_time)
                     "pic_size": return_size
                 }
-                # create_tmp function with json
-                make_tmp(new_info)
-                # input is (processed_list)
                 return jsonify(new_info)
 
 
