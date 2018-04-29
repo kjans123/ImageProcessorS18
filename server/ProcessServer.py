0a6e63fa843427e04516d44ba6ce3e1bfd6d98ec
kjans123 <kj123@duke.edu>
2018-04-29 15:09:49 -0400

    editing glob path

--- server/ProcessServer.py
+++ server/ProcessServer.py
@@ -74,7 +74,7 @@ def process():
     except TypeError:
         print("Please provide information in string format!")
         return jsonify("email is not string"), 400
-    jpgList = glob.glob("images/"+str(email)+"/.*")
+    jpgList = glob.glob("images/"+str(email)+"/*")
     jpgCount = len(jpgList)
     print(jpgCount)
     current_time = datetime.datetime.now()
