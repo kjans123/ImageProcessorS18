94d885cc826a0680b45f6f5983458702e695bf7a
kjans123 <kj123@duke.edu>
2018-04-29 13:51:18 -0400

    trying to resolve permission errors with mkdir

--- server/ProcessServer.py
+++ server/ProcessServer.py
@@ -85,7 +85,7 @@ def process():
     for i, img in enumerate(pre_img):
         if method == "Histogram Equalization":
             if jpgFileNum == 0:
-                os.mkdir(('/images/'+str(email)), mode=0o777)
+                os.mkdir(('/images/'+str(email)))
             jpgFileNum = jpgFileNum + 1
             filename = '/images/'+str(email)+'/'+str(jpgFileNum)+'.jpg'
             with open(filename, "wb") as image_out:
