18458bd34e0fba75c70cdc228fa281adc015d835
kjans123 <kj123@duke.edu>
2018-04-29 16:09:16 -0400

    extracting out mime string from base64 string

--- server/ProcessServer.py
+++ server/ProcessServer.py
@@ -84,10 +84,10 @@ def process():
     pre_img_histograms = []
     jpgFileNum = jpgCount
     for i, img in enumerate(pre_img):
-        print(img)
-        text_file = open("Output.txt", "w")
-        text_file.write(img)
-        text_file.close()
+        img = img.split(',',1)
+        print img[0]
+        #print img[1]
+        img = img[1]
         img = img.encode('utf-8')
         if method == "Histogram Equalization":
             if jpgFileNum == 0:
